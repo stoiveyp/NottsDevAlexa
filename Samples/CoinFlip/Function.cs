517744c2400905b2c62dcb7d7326a68b866804c9
Steven Pears <steven.pears@live.co.uk>
2017-09-03 00:17:47 +0100

    Add Coin Flip sample

---
+++ Samples/CoinFlip/Function.cs
@@ -0,0 +1,29 @@
+using System;
+using Alexa.NET;
+using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+using Alexa.NET.Response;
+using Amazon.Lambda.Core;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace CoinFlip
+{
+    public class Function
+    {
+        public SkillResponse FunctionHandler(SkillRequest request, ILambdaContext context)
+        {
+            if (request.GetRequestType() != typeof(LaunchRequest))
+            {
+                return ResponseBuilder.Empty();
+            }
+
+            var result = new Random().Next(0, 1) == 0 ? "head" : "tails";
+            var resultText = $"Certainly. And the result is. {result}";
+
+            var speech = new PlainTextOutputSpeech {Text = resultText};
+
+            return ResponseBuilder.Tell(speech);
+        }
+    }
+}
